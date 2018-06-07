f2c9e6eb44e66a7cdb96af6af97d3b315df01de0
abejith <abejith@outlook.com>
2018-06-08 01:07:56 +0530

    Client dependencies

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,4 +1,4 @@
-onst HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
